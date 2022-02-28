6151d294267b72b66db87d8214c2d8054174ca66
Oluwafikayo Ajayi <oluwafikayoajayi@gmail.com>
2022-02-28 23:11:08 +0100

    API module config implementation

---
+++ config/config.js
@@ -0,0 +1,26 @@
+let config = require("./config.json");
+class Config {
+
+    //API SETTINGS
+    static get get_app_name() {
+        return config.app_name;
+    }
+
+    static get get_app_port() {
+        return config.port;
+    }
+
+    static get get_app_version() {
+        return config.app_version;
+    }
+
+    static get get_api_version() {
+        return config.api_version;
+    }
+
+    static get get_config() {
+        return config;
+    }
+}
+
+module.exports = Config;
